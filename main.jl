90dd7cbe2e04146316273c567c22179f76531668
isitusnoamow <240135@hkis.edu.hk>
2022-10-15 00:07:27 +0800

    forgot to save

--- main.jl
+++ main.jl
@@ -1,3 +1,12 @@
 print("Enter Base 10 Number: "); 
-decimal = parse(Float64,readline());
-integer = trunc(Int64,decimal)
+nonint = false;
+decimalstr = readline();
+decimal = parse(Float64,decimalstr);
+integer = trunc(Int64,decimal);
+if length(split(decimalstr,".")) > 1
+    fraction = "." * split(decimalstr,".")[2];
+    nonint = true;
+    println(fraction);
+end
+b13integer = string(integer,base=13);
+println(b13integer);
