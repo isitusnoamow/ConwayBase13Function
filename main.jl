c36830ff2ef56b1c989356c47d9e839bb52163d1
isitusnoamow <240135@hkis.edu.hk>
2022-10-15 23:54:32 +0800

    fraction

--- main.jl
+++ main.jl
@@ -1,12 +1,15 @@
 print("Enter Base 10 Number: "); 
 nonint = false;
 decimalstr = readline();
+ending = "";
 decimal = parse(Float64,decimalstr);
 integer = trunc(Int64,decimal);
 if length(split(decimalstr,".")) > 1
     fraction = "." * split(decimalstr,".")[2];
+    fraction = parse(Float64,fraction);
     nonint = true;
     println(fraction);
+    original = fraction;
 end
 b13integer = string(integer,base=13);
 println(b13integer);
