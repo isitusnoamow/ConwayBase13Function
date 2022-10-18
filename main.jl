b35574f78406701b572c5dd8f532ec694043c48a
isitusnoamow <240135@hkis.edu.hk>
2022-10-18 23:18:09 +0800

    conway

--- main.jl
+++ main.jl
@@ -1,15 +1,28 @@
-print("Enter Base 10 Number: "); 
-nonint = false;
-decimalstr = readline();
-ending = "";
-decimal = parse(Float64,decimalstr);
-integer = trunc(Int64,decimal);
-if length(split(decimalstr,".")) > 1
-    fraction = "." * split(decimalstr,".")[2];
-    fraction = parse(Float64,fraction);
-    nonint = true;
-    println(fraction);
-    original = fraction;
+using Decimals
+
+print("Enter Base 10 Number: ")
+
+function splitNum(x)
+    splitted = split(x,".")
+    if length(splitted) > 1
+        return [splitted[1],"."*splitted[2]]
+    end
+    return splitted
 end
-b13integer = string(integer,base=13);
-println(b13integer);
+
+decimalstr = readline()
+fractional = "."
+if length(splitNum(decimalstr)) > 1
+    fraction = splitNum(decimalstr)[2]
+    fraction = decimal(fraction)
+    for i  in 1:20
+        global fraction *= 13
+        global fractional = fractional * string(parse(Int64,splitNum(string(fraction))[1]),base=13)
+        fraction = decimal(splitNum(string(fraction))[2])
+    end
+end
+b13integer = string(parse(Int64,splitNum(decimalstr)[1]),base=13)
+b13number = b13integer * fractional
+conwayb13number = replace(b13integer,"a"=>"+","b"=>"-","c"=>".")
+println(b13number)
+println(conwayb13number)
